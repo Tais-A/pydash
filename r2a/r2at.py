588f6cf821ff091a1f01c55e9bb828abd43659c1
TaisAlves <58663757+Tais-A@users.noreply.github.com>
2023-01-22 20:51:24 -0300

    Add files via upload

--- r2a/r2at.py
+++ r2a/r2at.py
@@ -1,3 +1,5 @@
+
+
 from r2a.ir2a import IR2A
 from abc import ABCMeta, abstractmethod
 from base.message import Message, MessageKind
@@ -14,11 +16,12 @@ class R2AT(IR2A):
         self.throughputs = []
         self.throughputs_peso = []
         self.request_time = 0
-        self.qi = [] 
-        self.contador = 0
-        self.index = 15
+        self.qi = [] #Vetor que armazena qualidade dos video 
+        self.contador = 0 #Descreve quantas requisições já foram, totalizando 597 
+        self.index = 15 #Usuários consideram menos prejudicial esperar no começo, sendo assim foi considerado uma qualidade boa, mas não o máximo para que essa espera não seja grande
         self.buffer_size = 0
         self.whiteboard = Whiteboard.get_instance()
+        self.listMI = []
  
 
 
@@ -46,12 +49,12 @@ class R2AT(IR2A):
             self.buffer_size = self.whiteboard.get_playback_buffer_size()[-1][1]
         else:
             self.buffer_size = 0
-        print(f" -----------------------> select = {self.index} ---- Size_request")
+
         if self.buffer_size >= 40 and self.index < 19:
             self.index += 1
-        elif self.buffer_size <= 10 and self.index > 0:
+        if self.buffer_size <= 10 and self.index > 0:
             self.index -= 1
-        print(f" -----------------------> select = {self.index} buffer = {self.buffer_size}---- Size_request")
+
         selected_qi = self.qi[self.index]
 
         msg.add_quality_id(self.qi[self.index])
@@ -65,47 +68,69 @@ class R2AT(IR2A):
         tamanho_bits = msg.get_bit_length()
         vazao = tamanho_bits/tempo
 
+        #Média da taxa de transferência
         self.throughputs.append(vazao)
         mi = sum(self.throughputs)/self.contador
+        self.listMI.append(mi)
 
-        # para priorizar throughtputs recentes em comparação com os mais velhos, definimos 
-        self.throughputs_peso.append(1/self.contador * (vazao - mi))
-        sigma = sum(self.throughputs_peso)
+        #Média com peso, usada para priozar throughout mais recentes. 
+        #self.throughputs_peso.append((1/self.contador) * abs(vazao - mi))
+        print(self.contador)
+        # if vazao != mi: 
+        #     self.throughputs_peso.append(1/(self.contador * abs(vazao - mi)))
+        #self.throughputs_peso.append(self.contador/596 * abs(vazao - mi))
+        i = 0
+        sigmaList = []
+        for item in self.throughputs:
+            sigmaList.append((i/self.contador) * abs(item - mi))
+            i += 1
+        #self.throughputs_peso.append(self.contador * abs(vazao - mi))
+        
+        # sigma = sum(self.throughputs_peso)
+        sigma = sum(sigmaList)
 
+
+        # P é a probabilidade de que 
         p = mi/(mi+sigma)
         desc = (1 - p) * self.qi[max(0, self.index-1)]
         cres = p * self.qi[min(19, self.index+1)]
+        
+        array = numpy.array(self.qi)
+        difference_array = []
+        for i in range(len(self.qi)):
+            if self.qi[i] < (desc + cres):
+                pass
+            else: 
+                index = i
+                break
+
+        # difference_array2 = numpy.absolute(array - desc + cres)
+        # index = difference_array2.argmin()
+
+        # print(difference_array)
+        # print(difference_array2)
+        #index = numpy.argmin(lst - abs(desc + cres))
 
 
-
-        lst = numpy.asarray(self.qi)
-        index = (numpy.abs(lst - desc + cres)).argmin()
-
-        print(f" -----------------------> select = {self.index} ---- Size_Response")
-
-        if self.buffer_size >= 50 and self.index < 19:
-            self.index += 1
+        if self.buffer_size >= 40 and self.index < 19:
+            self.index = index + 1
         elif self.buffer_size <= 10 and self.index > 0:
-            self.index -= 1
+            self.index = index - 1
+        # else:
+        #     self.index = index
+        # else:
+        #     if index < 19 and self.buffer_size >= 30:
+        #         self.index = index + 1
+        #     else:
         else:
             self.index = index
 
-        print(f" -----------------------> select = {self.index} index = {index} buffer = {self.buffer_size} ---- Size_Response")
-
-
-
-        print("")
-        print(f">>>>>>>>>>>>>>>>>> Tempo = {tempo}")
-        print(f">>>>>>>>>>>>>>>>>> Tamanho em bits = {tamanho_bits}")
-        print(f">>>>>>>>>>>>>>>>>> Vazao = {vazao}")
-        print(f">>>>>>>>>>>>>>>>>> Contador = {self.contador}")
-
-        print(f"select = {self.index} ---- {self.qi[self.index]}")
-
-        print("")
+        print(f"INDEX SELECT = {index}")
+        # print(lst - desc + cres)
+        # print(lst)
 
         self.send_up(msg)
-        # pass
+
 
 
     def initialize(self):
