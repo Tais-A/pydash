896d918d55cf1eae3e824fbfda257eef54de5b61
TaisAlves <58663757+Tais-A@users.noreply.github.com>
2023-01-29 01:01:24 -0300

    Update r2at.py

--- r2a/r2at.py
+++ r2a/r2at.py
@@ -1,6 +1,14 @@
+# Alunos:
+    # Tais Alves Oliveira - 190117176
+    # 
 
+# Disciplina:
+    # Dep. Ciência da Computação - Universidade de Brasília (UnB),
+    # Redes de Computadores - 2022.2
 
-from r2a.ir2a import IR2A
+# Implementação:
+    # > Finalization module Player
+
 from abc import ABCMeta, abstractmethod
 from base.message import Message, MessageKind
 from base.whiteboard import Whiteboard
@@ -13,105 +21,76 @@ class R2AT(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
+
         self.throughputs = []
-        self.throughputs_peso = []
+        self.throughput = 0
         self.request_time = 0
-        self.qi = [] #Vetor que armazena qualidade dos video 
-        self.contador = 0 #Descreve quantas requisições já foram, totalizando 597 
-        self.index = 15 #Usuários consideram menos prejudicial esperar no começo, sendo assim foi considerado uma qualidade boa, mas não o máximo para que essa espera não seja grande
-        self.buffer_size = 0
+        self.qi = []
+  
+        self.index = 4 
+
         self.whiteboard = Whiteboard.get_instance()
-        self.listMI = []
- 
+
+        self.uso_buffer = 0
 
 
     def handle_xml_request(self, msg):
-        self.request_time = time.perf_counter()
-        self.send_down(msg)
 
+        self.send_down(msg)
 
     def handle_xml_response(self, msg):
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
+
         self.send_up(msg)
         
-
-
     def handle_segment_size_request(self, msg):
+        self.request_time = time.perf_counter()
 
-        tempo = (time.perf_counter() - self.request_time)
-        vazao = (msg.get_bit_length()/tempo)
-        self.contador += 1
-        
-
-        #buffer
+        #atribui tamanho do buffer:
         if len(self.whiteboard.get_playback_buffer_size()) > 0:
-            self.buffer_size = self.whiteboard.get_playback_buffer_size()[-1][1]
+            buffer_size = self.whiteboard.get_playback_buffer_size()[-1][1]
         else:
-            self.buffer_size = 0
+            buffer_size = 0
+        
+        #Verifica se o buffer foi usado e quanto tempo levou para isso:
+        buffer = False
+        tamanho = len(self.whiteboard.get_playback_segment_size_time_at_buffer())
+        if tamanho > 0 :
+            tempo_espera = self.whiteboard.get_playback_segment_size_time_at_buffer()[-1]
+            if self.uso_buffer < tamanho:
+                self.uso_buffer = tamanho
+                buffer = True
+            else:
+                buffer = False
+        else:
+            tempo_espera = 1
+            
 
-        if self.buffer_size >= 50 and self.index < 19:
-            self.index += 1
-        if self.buffer_size <= 10 and self.index > 0:
-            self.index -= 1
-
-        selected_qi = self.qi[self.index]
+        #Ajusta o index e retorna um numero valido
+        self.index = retorna_index_valido(self.index + ajuste_buffer(buffer_size, tempo_espera, buffer))
 
         msg.add_quality_id(self.qi[self.index])
+
         self.send_down(msg)
 
 
     def handle_segment_size_response(self, msg):
-        print(msg.get_url())
 
-        tempo = time.perf_counter() - self.request_time
-        tamanho_bits = msg.get_bit_length()
-        vazao = tamanho_bits/tempo
+        # Adiciona a vazão atual na lista
+        self.throughput = msg.get_bit_length()/(time.perf_counter() - self.request_time) 
+        self.throughputs.append(self.throughput)
 
-        #Média da taxa de transferência
-        self.throughputs.append(vazao)
-        mi = sum(self.throughputs)/self.contador
-        self.listMI.append(mi)
+        momento = msg.get_segment_id()
+        media = mi(self.throughputs, momento)
+        peso = sigma(self.throughputs, momento, media)
+        probabilidade = p(media, peso)
 
-        #Média com peso, usada para priozar throughout mais recentes. 
-        i = 0
-        sigmaList = []
-        for item in self.throughputs:
-            sigmaList.append((i/self.contador) * abs(item - mi))
-            i += 1
-       
-        sigma = sum(sigmaList)
+        k = tau(probabilidade, self.qi, self.index) + teta(probabilidade, self.qi, self.index) # k é a constante dada somando a probabilidade da qualidade aumentar ou diminuir
 
-
-        # P é a probabilidade de que 
-        p = mi/(mi+sigma)
-        desc = (1 - p) * self.qi[max(0, self.index-1)]
-        cres = p * self.qi[min(19, self.index+1)]
-        
-        array = numpy.array(self.qi)
-        difference_array = []
-        for i in range(len(self.qi)):
-            if self.qi[i] < (desc + cres):
-                pass
-            else: 
-                index = i
-                break
-        
-
-        if self.buffer_size >= 40 and self.index < 19:
-            self.index = index + 1
-        elif self.buffer_size <= 10 and self.index > 0:
-            self.index = index - 1
-        else:
-            self.index = index
-
-        print(f"INDEX SELECT = {index}")
-        # print(lst - desc + cres)
-        # print(lst)
+        self.index = index_da_menor_diferenca(self.qi, k)
 
         self.send_up(msg)
-
-
 
     def initialize(self):
         #SimpleModule.initialize(self)
@@ -120,3 +99,77 @@ class R2AT(IR2A):
     def finalization(self):
         #SimpleModule.finalization(self)
         pass
+
+
+def mi(lista, momento):
+    # Média da taxa de transferencia
+    return sum(lista)/momento
+
+
+def sigma(lista, momento, media):
+    # Peso usado para priozar vazões mais recentes. 
+    i = 1
+    sigmaList = []
+    for item in lista:
+        sigmaList.append((i/momento) * abs(item - media))
+        i += 1
+       
+    return sum(sigmaList)
+
+
+def p(sigma, mi):
+    # Probalidade entre 0 e 1 que estima a disposição de mudar de qualidade:
+    # Um p proximo de 1, mostra uma rede estável 
+    # um p proximo de 0, mostra uma rede instável
+    return mi/(mi+sigma)
+
+
+def tau(p, qualidades, index):
+    #se o index não for 0, analisa a possibilidade da qualidade de descrescer
+    if index != 0:
+        index -= 1
+    return (1-p) * qualidades[index]
+
+
+def teta(p, qualidades, index):
+    #se o index não for 19, analisa a possibilidade da qualidade de crescer
+    if index != 19:
+        index += 1
+    return p * qualidades[index]
+
+
+def index_da_menor_diferenca(lista, k):
+    #compara todos os elementos da lista de qualidades com a constante estabelecida e devolve a menor diferença
+    lista_comparativa = []
+    for item in lista:
+        lista_comparativa.append(abs(item - k))
+    return lista_comparativa.index(min(lista_comparativa))
+
+
+def ajuste_buffer(buffer_size, tempo_espera,buffer):
+    #Comparando o buffer e a ultima mudança que teve na lista de buffer, cria um numero para ajustar o index para mais ou para menos
+    ajuste_buffer = -1
+    if buffer_size < 10:
+        ajuste_buffer -= 1
+        if buffer_size < 5:
+            ajuste_buffer -= 1
+    elif buffer_size > 30:
+        ajuste_buffer += 1
+        if buffer_size > 45:
+            ajuste_buffer += 1
+            if buffer_size > 55:
+                ajuste_buffer += 2
+    if buffer:
+        ajuste_buffer -= 1
+        if tempo_espera < 1:
+            ajuste_buffer -= 1
+        
+    return ajuste_buffer
+
+def retorna_index_valido(index):
+    #Avalia se o Index está dentro da lista, se não retorna indice da extremidade
+    if index < 0:
+        index = 0
+    elif index > 19:
+        index = 19
+    return index
